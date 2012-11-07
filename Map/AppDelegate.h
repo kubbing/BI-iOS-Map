e2c2706ecd090fcfa6d61b662a58a77fa88dc323
Jakub Hladik <kubbing@me.com>
2012-11-07 14:39:39 +0100

    Initial Commit

---
+++ Map/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Map
+//
+//  Created by Jakub Hladík on 07.11.12.
+//  Copyright (c) 2012 Jakub Hladík. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Map/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Map
+//
+//  Created by Jakub Hladík on 07.11.12.
+//  Copyright (c) 2012 Jakub Hladík. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
